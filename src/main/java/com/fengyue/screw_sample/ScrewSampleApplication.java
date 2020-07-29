6201faf2b01ac19f3d4b4ed92114838a7136942c
fy249085 <fy249085@gongdao.com>
2020-07-29 15:27:30 +0800

    first commit

---
+++ src/main/java/com/fengyue/screw_sample/ScrewSampleApplication.java
@@ -0,0 +1,13 @@
+package com.fengyue.screw_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrewSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrewSampleApplication.class, args);
+	}
+
+}
